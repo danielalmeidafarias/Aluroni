c41e7885c109e61031ab8b1b60d29b7c9cc5e803
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2023-11-08 15:46:38 -0300

    adicionando eslint

--- src/pages/cardapio/buscador/index.tsx
+++ src/pages/cardapio/buscador/index.tsx
@@ -1,20 +1,21 @@
-import styles from './Buscador.module.scss'
-import { CgSearch } from 'react-icons/cg'
+import React from 'react';
+import styles from './Buscador.module.scss';
+import { CgSearch } from 'react-icons/cg';
 
 interface Props {
-    busca: string,
-    setBusca:  React.Dispatch<React.SetStateAction<string>>
+  busca: string, 
+  setBusca:  React.Dispatch<React.SetStateAction<string>>
 }
 
 const Buscador = ({ busca, setBusca } : Props) => {
-    return (  
-        <div className={styles.buscador}>
-            <input placeholder='Buscar' type="text" value={busca} onChange={(e) => {
-                setBusca(e.target.value)
-            }}/>
-            <CgSearch size={20} color='#4c4d5e'/>
-        </div>
-    );
-}
+  return (  
+    <div className={styles.buscador}>
+      <input placeholder='Buscar' type="text" value={busca} onChange={(e) => {
+        setBusca(e.target.value);
+      }}/>
+      <CgSearch size={20} color='#4c4d5e'/>
+    </div>
+  );
+};
  
 export default Buscador;
--- src/pages/cardapio/filtros/index.tsx
+++ src/pages/cardapio/filtros/index.tsx
@@ -1,6 +1,6 @@
-import filtros from './filtros.json'
-import styles from './Filtros.module.scss'
-import classNames from 'classnames'
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+import classNames from 'classnames';
 
 type Opcao = typeof filtros[0]
 
@@ -10,25 +10,25 @@ interface Props {
 }
 
 const Filtros = ({ filtro, setFiltro }: Props) => {
-    function selecionarFiltro(opcao: Opcao) {
-        if(filtro === opcao.id) {
-            return setFiltro(null)
-        }
-        return setFiltro(opcao.id)
+  function selecionarFiltro(opcao: Opcao) {
+    if(filtro === opcao.id) {
+      return setFiltro(null);
     }
+    return setFiltro(opcao.id);
+  }
 
-    return (  
-        <div className={styles.filtros}>
-            {filtros.map((opcao) => (
-                <button className={classNames({
-                    [styles.filtros__filtro]: true,
-                    [styles['filtros__filtro--ativo']]: filtro === opcao.id
-                })} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
-    );
-}
+  return (  
+    <div className={styles.filtros}>
+      {filtros.map((opcao) => (
+        <button className={classNames({
+          [styles.filtros__filtro]: true,
+          [styles['filtros__filtro--ativo']]: filtro === opcao.id
+        })} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  );
+};
  
 export default Filtros;
