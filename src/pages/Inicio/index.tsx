b3f77d6310f589e3ed0f16e7df6a6dc3ca5f28cd
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2023-11-09 22:28:52 -0300

    modificando h2 inicio

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -18,7 +18,7 @@ const Inicio = () => {
   return (  
     <section>
       <h2 className={stylesTema.titulo}>
-        Recomendações da Cozinha
+        Recomendações do Chefe
       </h2>
       <div className={styles.recomendados}>
         {pratosRecomendados.map(item => (
